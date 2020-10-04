b7a5fe29878c08b0fe0c0c7339f9f2cdaede64b3
Anant Bharti <57601502+anantbharti@users.noreply.github.com>
2020-10-04 20:43:47 +0530

    Update main.py

--- JourneyToTheMoon/main.py
+++ JourneyToTheMoon/main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
     
-N, I = map(int, raw_input().strip().split())
+N, I = map(int, input().strip().split())
 assert 1 <= N <= 10**5
 assert 1 <= I <= 10**6
 lis_of_sets = []
     
 for i in range(I):
-    a,b = map(int, raw_input().strip().split())
+    a,b = map(int, input().strip().split())
     assert 0 <= a < N and 0 <= b < N
     indices = []
     new_set = set()
@@ -29,4 +29,4 @@ answer = N*(N-1)/2
 for i in lis_of_sets:
     answer -= len(i)*(len(i)-1)/2
     
-print answer
+print(answer)
